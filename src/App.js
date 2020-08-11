6c69662e2317e30110b86209cca1c19687c442ec
Priyam <priyam009@live.com>
2020-08-11 20:52:26 +1000

    Add Particle js component and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./App.css";
 
+import Particles from "react-particles-js";
+import background from "./utils/particles.utils";
+
 import Header from "./components/header/header.components";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      background: background,
+    };
   }
 
   render() {
     return (
       <div>
-        <Header />
+        <Particles className="particles" params={this.state.background} />
+        <div className='container'>
+          <Header />
+        </div>
       </div>
     );
   }
